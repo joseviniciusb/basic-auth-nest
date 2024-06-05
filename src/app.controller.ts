6d5c0c950c825516b9e0ccb4552d350649241934
josé <jose@pulsarmultimedia.com>
2024-06-05 10:56:45 -0300

    feat: create a basic auth in nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('basic'))
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  findOne() {
+    console.log('lalaland');
+  }
+}
